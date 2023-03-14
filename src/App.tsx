de713f06a11a638c736c438a4158de844a0de9a0
Gennadi Kudrjavtsev <gennadi@agrello.org>
2023-03-14 13:47:12 +0200

    added web-eid and simple usage to show an issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { status } from '@web-eid/web-eid-library';
 
 function App() {
+
+  useEffect(() => {
+    status()
+    .then((result) => {
+      console.log('status:', result)
+    })
+    .catch((error) => {
+      console.error('error:', error)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
